6a8b6f35302cfa32833cb050a129143117b68476
No Man's Slave <foo@example.net>
2020-12-11 03:25:36 -0500

    Add descriptions

--- setup.py
+++ setup.py
@@ -4,11 +4,14 @@ setup(
     name='tsquery',
     version='0.1.1',
 
-    url='https://sr.ht/~wintershadows/tsquery',
-
     author='Greg Werbin',
     author_email='software@me.gregwerbin.com',
     license='GPL-3.0-or-later',
+
+    url='https://sr.ht/~wintershadows/tsquery',
+    description='Run Tree Sitter queries from the command line',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
 
     package_dir={'': 'src'},
     packages=find_packages('src'),
